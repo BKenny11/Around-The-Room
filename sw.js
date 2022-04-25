1b0e1a9137a406e183635318b370a06c19566d9f
Brendan <brendan.w.kenny@gmail.com>
2022-04-24 22:22:44 -0400

    CSS fixes v03

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ var APP_PREFIX = 'atr_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_02';
+var VERSION = 'version_03';
 
 var CACHE_NAME = APP_PREFIX + VERSION
  
